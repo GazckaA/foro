2fed1cc6fa15a612ce2f167e87e3adb885bb3bb4
Ian Gazcka Arechiga <iangazcka@gmail.com>
2024-05-13 17:07:54 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Foro Filosofia',
+  webDir: 'www'
+};
+
+export default config;
